fc1b6c267dfc53c165f912713422b6c3beaa1ae4
Arpitha <arpitha.sureshappa@tarento.com>
2026-03-12 18:14:47 +0530

    KB-13480 badegDetails new key added. (#212)

--- src/main/java/com/igot/cb/pores/util/Constants.java
+++ src/main/java/com/igot/cb/pores/util/Constants.java
@@ -450,7 +450,7 @@ public class Constants {
     public static final String LOG_ES_UPDATE_FAILURE = "ES update failed for contentId={}";
     public static final String LOG_DB_BULK_UPDATE_SUCCESS = "DB bulk update successful for receivedContentIdsSize={} and updatedRecordsCount={}";
     public static final String LOG_DB_BULK_UPDATE_FAILURE = "DB bulk update failed for contentIds={}";
-
+    public static final String BADGE_DETAILS_V1 = "badgeDetails_v1";
 
 
     private Constants() {
